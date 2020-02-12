359783ac168058e268dca41057f3de12b582af63
Brice Djilo <brice.djilo@endurance.com>
2020-02-12 00:52:00 -0700

    Updates

---
+++ precache-manifest.eb4f284344efcc5ccdda432535556b9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68ca61f9536eba57786e36b47767124",
+    "url": "/monsters-rolodex/public/index.html"
+  },
+  {
+    "revision": "461792231668529f8d67",
+    "url": "/monsters-rolodex/public/static/css/main.6d461bb3.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/public/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/public/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "461792231668529f8d67",
+    "url": "/monsters-rolodex/public/static/js/main.8d1bd3a7.chunk.js"
+  },
+  {
+    "revision": "e216014c357fa7cb4452",
+    "url": "/monsters-rolodex/public/static/js/runtime-main.46a19679.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/public/static/media/logo.5d5d9eef.svg"
+  }
+]);
